6280ddf7ade7b71b2e9951849216fc6883a55d1c
Gourav Tyagi <gourav@inkhive.com>
2017-08-17 14:39:05 +0530

    Latest Switch to new framework

---
+++ _social-fa.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gourav
+ * Date: 8/17/2017
+ * Time: 2:33 PM
+ */
--- modules/social/social-fa.php
+++ modules/social/social-fa.php
@@ -1,4 +1,12 @@
-<div id="social-icons">
-		<?php get_template_part('social', 'fa'); ?>
-		<div id="top-search-form"><?php get_search_form(); ?></div>
-	</div>
+<?php
+/*
+** Template to Render Social Icons on Top Bar
+*/
+
+for ($i = 1; $i < 8; $i++) :
+	$social = esc_attr( get_theme_mod('nevler_social_'.$i) );
+	if ( ($social != 'none') && ($social != '') ) : ?>
+	<a class="social-icon hvr-bounce-to-bottom" href="<?php echo esc_url( get_theme_mod('nevler_social_url'.$i) ); ?>"><i class="fa fa-<?php echo $social; ?>"></i></a>
+	<?php endif;
+
+endfor; ?>
